0e1914512bd229d64778465d0223329eee1017c4
Franscesca Ark <fransces@uoregon.edu>
2024-07-04 11:45:51 -0700

    committing how I do batches of data generation in runDataAndAnalysis, before I completely change how I save data due to calculating stats inside the generator loop

--- runDataAndAnalysis.py
+++ runDataAndAnalysis.py
@@ -5,26 +5,37 @@ import numpy as np
 
 # 8 April changing all dirichlet --> distribution and getRoughness to getRoughnessNew... add params
 # 9 April getRoughnessNew --> getRoughness
+#TODO: fix to incorporate that roughness now computed w/in the loop
 def runDataAndAnalysis(directory, sysID, occupancy, MaxT, distribution, params, PDF):
     # check if path exists or create if doesnt; also create ANOTHER directory to throw statistics into
+    print("top of runDataAndAnalysis PDF", PDF, type(PDF))
     path = f"{directory}"
     statsPath = f"{directory}"+"Statistics"
     if PDF:  # to better label directories
         path = path + "PDF"
-        statsPath = path + "Statistics"
+        # statsPath = path + "Statistics" (Don't need b/c stats in generator loop now)
     if not os.path.exists(path):
         os.mkdir(path)
-        os.mkdir(statsPath)
-        print(f"{path} and {statsPath} have been created.")
+        # os.mkdir(statsPath)
+        print(f"{path} has been created.")
 
-    # then generate the tArrival and occupancy array and save it
-    tArrival, occ = ev.generateFirstArrivalTime(occupancy, MaxT, distribution, params, PDF)
-    # save the tArrival and occ to the directory we created with the systID as the filename
-    np.savez_compressed(f"{path}/{sysID}.npz", tArrival=tArrival, occ=occ)
-
-    # #now I want to do the analysis...
-    statistics = ev.getRoughness(tArrival)  # use getRoughnessNew instead
-    np.save(f"{statsPath}/{sysID}.npy", statistics)  # since outputs one array, use np.save and .npy
+    if PDF:  # if evolving PDF then use evolvePDF, save the relevant stuff
+        print("right before ev.evolvePDF", PDF, type(PDF))
+        pdf, cdf, cdfStats = ev.evolvePDF(MaxT, distribution, params)
+        # save the PDF, CDF, & CDF Stats to the directory with the systID as the filename
+        np.savez_compressed(f"{path}/{sysID}.npz", pdf = pdf, cdf = cdf, cdfStats = cdfStats)
+        #TODO: rewrite once i split generateFirstArrival
+        # # now I want to do the analysis. on tArrival arrays (if agents)
+        # if not PDF:
+        #     statistics = ev.getRoughness(tArrival)
+        #     # statistics = ev.getTArrivalRoughness(tArrival) # 29 Apr testing new functions for get Rougheness
+        # else:  # if you are doing PDFs then we'll want the PDF roughness stats
+        #     statistics = ev.getRoughness(occ)
+        # np.save(f"{statsPath}/{sysID}.npy", statistics)  # since outputs one array, use np.save and .npy
+    else:  # if evolving agents then use evolveAgents, save relevant stuff
+        tArrival, occ, tArrStats = ev.evolveAgents(occupancy, MaxT, distribution, params)
+        np.savez_compressed(f"{path}/{sysID}.npz",tArrival = tArrival, occupancy = occ, tArrivalStats = tArrStats)
+        print(f"Saved {path}/{sysID}.npz")
 
 
 if __name__ == "__main__":
@@ -39,8 +50,8 @@ if __name__ == "__main__":
     # dirichlet = eval(sys.argv[5])  # Bool
     distribution = str(sys.argv[5])  # string for distribution name
     params = sys.argv[6]  # i think this needs to be a list
-    PDF = eval(sys.argv[7])  # Bool
-    numSystems = int(sys.argv[8])  # Integer
+    PDF = eval(str(sys.argv[7]))  # Bool
+    numSystems = int(sys.argv[8])  # Integer (why do I have this..)
 
     for i in range(numSystems):
         runDataAndAnalysis(directoryName, sysID + i, occupancy, MaxT, distribution, params, PDF)
