43896aca394cc54b9ed1acd93b006f3403580de8
Jacob Hass <jacobhass8@gmail.com>
2025-04-03 11:05:47 -0700

    Fixed hermitian matrices

--- runFiles/largestEigenValueHermitian.m
+++ runFiles/largestEigenValueHermitian.m
@@ -1,16 +1,16 @@
-function [] = largestEigenValueHermitian(dir, sysID)
+function [] = largestEigenValue(dir, sysID, seed, N, numSamples)
 directory = sprintf("%s", dir);
 cd(directory);
-
-N = 3;
-numSamples = 3;
+N = str2num(N);
+numSamples = str2num(numSamples);
 fileName = sprintf("%s/EigenValues%s.txt", dir, sysID);
-eigenValues = zeros(numSamples, 1);
-rng(str2num(sysID));
+eigenValues = zeros(numSamples, 2);
+
+rng(str2num(seed));
 
 A = normrnd(0, 1/2, N, N, N, numSamples) + 1i * normrnd(0, 1/2, N, N, N, numSamples);
 
-C = permute(A, [1 2 3 4]) + conj(permute(A, [2 1 3 4])) + (permute(A, [2 3 1 4])) + conj(permute(A, [3 2 1 4])) + (permute(A, [3 1 2 4])) + conj(permute(A, [1 3 2 4]));
+C = permute(A, [1 2 3 4]) + conj(permute(A, [2 1 3 4])) + (permute(A, [2 3 1 4])) + conj(permute(A, [3 2 1 4])) + permute(A, [3 1 2 4]) + conj(permute(A, [1 3 2 4]));
 C = C / 6;
 
 for i=1:N
@@ -19,11 +19,9 @@ end
 
 for i=1:numSamples
     arr = C(:, :, :, i);
-    lambda = eeig(arr);
-    disp(lambda);
-    realLambda = lambda(abs(imag(lambda)) < 10 * eps);
-    maxLambda = max(real(realLambda));
-    eigenValues(i) = maxLambda;
+    [lambda, V, res, cnd] = zeig(arr);
+    eigenValues(i, 1) = lambda(end);
+    eigenValues(i, 2) = res(end);
 end
 
 writematrix(eigenValues, fileName);
