2212d5fd7951fd1e3f5b9a755eb246b02439db5d
Jacob Hass <jacobhass8@gmail.com>
2025-05-09 12:13:31 -0700

    Fixed the variance calculation of the proposed steps

--- metropolis/tensorMetropolis.m
+++ metropolis/tensorMetropolis.m
@@ -1,5 +1,28 @@
-function prob = pij(xij)
-    prob = exp(-xij^2 / 2);
+outSteps = 100000;
+inSteps = 100;
+N = 10;
+f = exp(1);
+b = 0.92;
+nBins = 10;
+width = 6;
+binLambdaMin = 1;
+binLambdaMax = 8;
+[logWeights, nc, f, histogram, accepted, rejected] = metropolisEigenValues(outSteps, inSteps, N, f, b, nBins, width, binLambdaMin, binLambdaMax);
+writematrix(logWeights, "./MatlabWeights/Weights.txt");
+writematrix(histogram, "./MatlabWeights/Histogram.txt");
+
+function count = d(i, j, k)
+    if i == j && j==k
+        count = 1;
+    elseif i == j || j == k || i == k
+        count = 3;
+    else 
+        count = 6;
+    end
+end
+
+function prob = pijk(xijk, i, j, k)
+    prob = exp(-xijk^2 / 2 / d(i, j, k));
 end
 
 function bin = computeBin(lambdaC, binLambdaMin, binLambdaMax, nBins)
@@ -21,11 +44,29 @@ function [eigValues, eigVectors] = getEigenvalues(prevEigVectors, A)
     eigValues = zeros(numEigVectors, 1);
     eigVectors = zeros(size(prevEigVectors));
     
-    parfor eigIdx=1:numEigVectors
+    for eigIdx=1:numEigVectors
         [powerLambda, powerV] = eig_sshopm(tensor(A), 'Start', prevEigVectors(:, eigIdx));
         eigValues(eigIdx) = powerLambda;
         eigVectors(:, eigIdx) = powerV; 
     end
+end
+
+function A = getSymmetricTensor(N)
+    A = symtensor(@randn, 3, N);
+
+    for i=1:N
+        for j=1:i
+            for k=1:j
+                if i==j && j==k
+                    continue
+                elseif i==j || i==k || j==k
+                    A(i, j, k) = A(i, j, k) / 3;
+                else
+                    A(i, j, k) = A(i, j, k) / 6;
+                end
+            end
+        end
+    end 
 end
 
 function [logWeights, nc, f, histogram, accepted, rejected] = metropolisEigenValues(outSteps, inSteps, N, f, b, nBins, width, binLambdaMin, binLambdaMax)
@@ -33,10 +74,9 @@ function [logWeights, nc, f, histogram, accepted, rejected] = metropolisEigenVal
     histogram = zeros(nBins, 1);
     nc = 0;
 
-    A = symtensor(@randn, 3, N);
-    A = double(full(A));
+    A = getSymmetricTensor(N);
 
-    [lambda, eigVectors] = zeig(A);
+    [lambda, eigVectors] = zeig(double(full(A)));
 
     % Filter out negative lambda since they are just duplicates
     eigVectors = eigVectors(:, lambda > 0);
@@ -54,20 +94,14 @@ function [logWeights, nc, f, histogram, accepted, rejected] = metropolisEigenVal
             proposedJump = randn() * width / 6;
             
             Anew = A;
-            % Enforce all the symmetries of the tensor
-            Anew(i, j, k) = Anew(i, j, k) + proposedJump;
-            Anew(i, k, j) = Anew(i, k, j) + proposedJump;
-            Anew(k, i, j) = Anew(k, i, j) + proposedJump;
-            Anew(k, j, i) = Anew(k, j, i) + proposedJump;
-            Anew(j, i, k) = Anew(j, i, k) + proposedJump;
-            Anew(j, k, i) = Anew(j, k, i) + proposedJump;
+            Anew(i,j,k) = Anew(i, j, k) + proposedJump;
             
-            [lambdaNew, eigVectorsNew] = getEigenvalues(eigVectors, tensor(Anew));
+            [lambdaNew, eigVectorsNew] = getEigenvalues(eigVectors, Anew);
             lambdaMaxNew = max(lambdaNew);
 
             binLambdaNew = computeBin(lambdaMaxNew, binLambdaMin, binLambdaMax, nBins);
             binLambda = computeBin(lambdaMax, binLambdaMin, binLambdaMax, nBins);
-            monteCarloProbability = pij(Anew(i, j, k)) / pij(A(i, j, k)) * exp(logWeights(binLambdaNew) - logWeights(binLambda));
+            monteCarloProbability = pijk(Anew(i, j, k), i, j, k) / pijk(A(i, j, k), i, j, k) * exp(logWeights(binLambdaNew) - logWeights(binLambda));
 
             if isnan(monteCarloProbability)
                 throw(MException("Nan"));
@@ -86,7 +120,7 @@ function [logWeights, nc, f, histogram, accepted, rejected] = metropolisEigenVal
             logWeights(binLambda) = logWeights(binLambda) - log(f);
             histogram(binLambda) = histogram(binLambda) + 1;
         end
-        display([accepted, rejected]);
+        display([accepted / (accepted + rejected) * 100, lambdaMax]);
         choppedHistogram = histogram(2:end-1);
         % nonZeroChopped = choppedHistogram(choppedHistogram ~= 0);
 
@@ -101,16 +135,3 @@ function [logWeights, nc, f, histogram, accepted, rejected] = metropolisEigenVal
         
     end
 end
-
-outSteps = 100000;
-inSteps = 100;
-N = 10;
-f = exp(1);
-b = 0.92;
-nBins = 30;
-width = 6;
-binLambdaMin = 4.5;
-binLambdaMax = 16;
-[logWeights, nc, f, histogram, accepted, rejected] = metropolisEigenValues(outSteps, inSteps, N, f, b, nBins, width, binLambdaMin, binLambdaMax);
-writematrix(logWeights, "./MatlabWeights/Weights.txt");
-writematrix(histogram, "./MatlabWeights/Histogram.txt");
