9eb55d01fbcf21a64fcae11b7b1255e26fee4476
Franscesca Ark <fransces@uoregon.edu>
2025-10-01 16:53:09 -0700

    change velocities to do 0.1 to 0.6 in 0.05 increments, 0.61 to 1 in 0.01 increments

--- runFiles/runDirichlet.py
+++ runFiles/runDirichlet.py
@@ -25,7 +25,9 @@ if __name__ == '__main__':
 
 	ts = getListOfTimes(tMax - 1, 1)
 	#velocities = np.geomspace(10 ** (-3), 10, 21)
-	velocities = np.linspace(0.6, 0.8, 21)  # in 0.01 increments
+	# velocities = np.linspace(0.6, 0.8, 21)  # in 0.01 increments
+    # actually let's do 0.1 to 0.6 in 0.05 increments, and then 0.61 to 1 in 0.01 increments
+    velocities = np.concatenate( (np.linspace(0.1, 0.6, 11), np.linspace(0.61, 1, 41)) )
 
 	vars = {'L': L, 
 			'ts': ts,
