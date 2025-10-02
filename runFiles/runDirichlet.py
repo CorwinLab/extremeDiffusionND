a8cc8f58b5081966d41cb3217edbca742fe9b5a5
Franscesca Ark <fransces@uoregon.edu>
2025-10-01 17:20:04 -0700

    typos

--- runFiles/runDirichlet.py
+++ runFiles/runDirichlet.py
@@ -6,48 +6,42 @@ from time import time as wallTime
 from datetime import date
 
 if __name__ == '__main__':
-	# Test Code
+    # Test Code
 	# L, tMax, distName, params, directory, systID = 1000, 50, 'Dirichlet', '1,1,1,1', './', 0
+    L = int(sys.argv[1])
+    tMax = int(sys.argv[2])
+    distName = sys.argv[3]
+    params = sys.argv[4]
+    directory = sys.argv[5]
+    systID = int(sys.argv[6])
+    # Need to parse params into an array unless it is an empty string
+    if params == 'None':
+        params = ''
+    else:
+        params = params.split(",")
+        params = np.array(params).astype(float)
 
-	L = int(sys.argv[1])
-	tMax = int(sys.argv[2])
-	distName = sys.argv[3]
-	params = sys.argv[4]
-	directory = sys.argv[5]
-	systID = int(sys.argv[6])
-
-	# Need to parse params into an array unless it is an empty string
-	if params == 'None':
-		params = ''
-	else:
-		params = params.split(",")
-		params = np.array(params).astype(float)
-
-	ts = getListOfTimes(tMax - 1, 1)
-	#velocities = np.geomspace(10 ** (-3), 10, 21)
-	# velocities = np.linspace(0.6, 0.8, 21)  # in 0.01 increments
+    ts = getListOfTimes(tMax - 1, 1)
+    # velocities = np.geomspace(10 ** (-3), 10, 21)
+    # velocities = np.linspace(0.6, 0.8, 21)  # in 0.01 increments
     # actually let's do 0.1 to 0.6 in 0.05 increments, and then 0.61 to 0.99 in 0.01 increments, then 0.99 to 1 in 0.001 increments
     velocities = np.concatenate((np.linspace(0.1, 0.6, 11), np.linspace(0.61, 0.99,39), np.linspace(0.991, 1, 10)))
-
-	vars = {'L': L, 
+    vars = {'L': L,
 			'ts': ts,
 			'velocities': velocities,
 			'params': params,
 			'directory': directory,
 			'systID': systID}
-	os.makedirs(directory, exist_ok=True)  # without this, gets mad that directory might not fully exist yet
-	vars_file = os.path.join(directory, "variables.json")
-
-	today = date.today()
-	text_date = today.strftime("%b-%d-%Y")
-
-	# Only save the variables file if on the first system
-	if systID == 0:
-		vars.update({"Date": text_date})
-		saveVars(vars, vars_file)
-		vars.pop("Date")
-
-	start = wallTime()
-	# runDirichlet(L, ts, velocities, params, directory, systID)
-	runDirichlet(**vars)
-	print(wallTime() - start, flush=True)
+    os.makedirs(directory, exist_ok=True)  # without this, gets mad that directory might not fully exist yet
+    vars_file = os.path.join(directory, "variables.json")
+    today = date.today()
+    text_date = today.strftime("%b-%d-%Y")
+    # Only save the variables file if on the first system
+    if systID == 0:
+        vars.update({"Date": text_date})
+        saveVars(vars, vars_file)
+        vars.pop("Date")
+    start = wallTime()
+    # runDirichlet(L, ts, velocities, params, directory, systID)
+    runDirichlet(**vars)
+    print(wallTime() - start, flush=True)
